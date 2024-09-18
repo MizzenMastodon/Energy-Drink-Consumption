5e3926d334f79a9f361ce918d82c4bc2db73282d
MizzenMastodon <Nbaum0701@gmail.com>
2024-09-18 18:42:15 +0000

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main() { 
-  int polledCustomers = 16500;
+  int polledCustomers;
+  //gets number of polled customers
+  cout << "How many customers did you poll? ";
+  cin >> polledCustomers; 
+  //calculates and stores the percent of customers that purchased more than one a week as well as the number of those customers that purchased citrus-flavored drinks
   int percentPurchased = polledCustomers * 0.15;
-  int percentCitrus = percentPurchased * 0.58; //variables defined with information given in the problem statement
-
-  cout << "Out of " << polledCustomers << " polled customers, " << percentPurchased << " purchased one or more energy drinks per week. " << percentCitrus << " of those customers purchased citrus-flavored energy drinks. "; //outputs the information. 
+  int percentCitrus = percentPurchased * 0.58;
+  //outputs the information. 
+  cout << "Out of " << polledCustomers << " polled customers, " << percentPurchased << " purchased one or more energy drinks per week. " << percentCitrus << " of those customers purchased citrus-flavored energy drinks. "; 
 
   
 
